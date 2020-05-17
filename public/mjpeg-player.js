10f49f9f730d16f1235e4074e20c01a232f6e5b4
Johannes Rabausch <jrabausch@absatzformat.de>
2020-05-18 01:05:06 +0200

    Added socket prototype

--- public/mjpeg-player.js
+++ public/mjpeg-player.js
@@ -1,5 +1,81 @@
 (function(context){
 
+	var NEXT_FRAME = String.fromCharCode(0xe6);
+
+	var JpegSocket = function(callback){
+
+		this.callback = callback;
+		this.isPlaying = false;
+		this.url = '';
+		this.socket = null;
+	};
+
+	var jp = JpegSocket.prototype;
+
+	jp.connect = function(url){
+
+		this.url = url;
+		this.socket = new WebSocket(url); 
+		this.socket.binaryType = 'blob';
+		this.socket.addEventListener('open', this.socketOpen.bind(this));
+		this.socket.addEventListener('message', this.socketMessage.bind(this));
+		this.socket.addEventListener('close', this.socketClose.bind(this));
+	};
+
+	jp.socketOpen = function(e){
+		if(this.isPlaying){
+			this.socket.send(NEXT_FRAME);
+		}
+	};
+
+	jp.socketMessage = function(message){
+
+		if(typeof message.data === 'string'){
+			console.log(message.data);
+		}
+		else{
+			if(this.isPlaying && this.socket.readyState === WebSocket.OPEN){
+				this.socket.send(NEXT_FRAME);
+			}
+			typeof this.callback === 'function' && this.callback(message.data);
+		}
+	};
+
+	jp.socketClose = function(e){
+
+		this.isPlaying = false;
+	};
+
+	jp.reconnect = function(){
+
+		if(this.url.length){
+
+			this.connect(this.url);
+		}
+	};
+
+	jp.play = function(){
+		
+		if(!this.isPlaying){
+
+			this.isPlaying = true;
+
+			if(this.socket.readyState === WebSocket.OPEN){
+				this.socket.send(NEXT_FRAME);
+			}
+		}
+	};
+
+	jp.pause = function(){
+		this.isPlaying = false;
+	};
+
+	context.JpegSocket = JpegSocket;
+
+})(window);
+
+(function(context){
+
 	'use strict';
 
 	var NEXT_FRAME_MESSAGE = String.fromCharCode(0xe6);
@@ -68,7 +144,6 @@
 			console.log(data);
 		}
 		else{ // blob -> image data
-			URL.revokeObjectURL(this.preloadImage.src);
 			this.preloadImage.src = URL.createObjectURL(message.data);
 			this.loadedData += message.data.size;
 		}
@@ -78,7 +153,9 @@
 		if(e.type === 'load'){
 			this.drawFrame();
 		}
-		
+
+		URL.revokeObjectURL(this.preloadImage.src);
+
 		// request next frame
 		if(this.isPlaying && this.socket.readyState === WebSocket.OPEN){
 			this.socket.send(NEXT_FRAME_MESSAGE);
